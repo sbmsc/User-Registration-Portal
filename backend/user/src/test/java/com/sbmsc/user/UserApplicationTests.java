10a6ebf95e89571e9821af663dea507b1f62a35a
sbmsc <chauhanshubham089@gmail.com>
2021-06-25 11:35:35 +0530

    Setting up project folder

---
+++ backend/user/src/test/java/com/sbmsc/user/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbmsc.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
